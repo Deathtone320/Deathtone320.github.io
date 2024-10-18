b2814cc266a0e5edadc0d6d71c8f638e3ce5a289
Deathtone320 <beginmegaquest@gmail.com>
2024-10-18 09:07:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,18 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 
     // Send the email using EmailJS
     emailjs.send(
-    'service_z7ywxbv',  // Your service ID
-    'template_e6cnynh', // Your template ID
-    {
-        from_name: name,
-        from_email: email,
-        message: message
-    },
-    'mlw1gOv3vSlu-5g1m' // Your public API key
-)
-.then(function(response) {
-    console.log('SUCCESS!', response.status, response.text);
-}, function(error) {
-    console.log('FAILED...', error);
-});
+        'service_z7ywxbv',  // Your service ID
+        'template_e6cnynh', // Your template ID
+        {
+            from_name: name,
+            from_email: email,
+            message: message
+        },
+        'mlw1gOv3vSlu-5g1m' // Your public API key
+    )
+    .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+        console.log('FAILED...', error);
+    });
+}); 
